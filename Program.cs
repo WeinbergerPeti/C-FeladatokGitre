a4a56e71d6212a9abd593175e2fa62d77e87f930
WeinbergerPeti <weinbergerpeti@gmail.com>
2024-12-15 13:00:17 +0100

    F02 feladat elkészítve

--- Program.cs
+++ Program.cs
@@ -11,12 +11,29 @@ namespace GitFeladatok
 		static void Main(string[] args)
 		{
 			int[] szamok = new int[10];
-			F01();
+			F01(szamok);
 		}
 
 		static void F01(int[] tomb)
 		{
 			Random random = new Random();
+			for (int i = 0; i < tomb.Length; i++)
+			{
+				tomb[i] = random.Next(1, 51);
+			}
+		}
+
+		static int F02(int[] tomb)
+		{
+			int szamlalo = 0;
+			for (int i = 0; i < tomb.Length; i++)
+			{
+				if (tomb[i] % 2 == 0)
+				{
+					szamlalo++;
+				}
+			}
+			return szamlalo;
 		}
 	}
 }
