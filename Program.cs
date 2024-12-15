09562711d9e3df561510052516df7cb803cd150d
WeinbergerPeti <weinbergerpeti@gmail.com>
2024-12-15 12:52:48 +0100

    F01 feladat

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFeladatok
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] szamok = new int[10];
+			F01();
+		}
+
+		static void F01(int[] tomb)
+		{
+			Random random = new Random();
+		}
+	}
+}
